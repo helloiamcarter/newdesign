bd723b8afd7e082aaa85b72de13c6b3f976618e6
helloiamcarter <great.carterisgreat@gmail.com>
2015-02-15 15:55:25 -0600

    changed php

--- finaldesign/cosmetic/tattoo-removal.php
+++ finaldesign/cosmetic/tattoo-removal.php
@@ -1,12 +1,12 @@
 <?php
 	$links = file_get_contents("../include.txt");
-	$links = explode("~",$includes);
+	$includes = explode("~",$links);
 	echo $includes[0]; ?>
 	
 			<div id="text">
 				<h2>Tattoo Removal</h2>
 				<p>Tattoo Removal performed by our RevLite Laser & PicoSure Laser system is now one of the most effective methods for tattoo removal on the market. Allow us to eliminate tattoo regret today through a series of treatments at Canopy MD.</p> 
-				<p>The laser works to break down the pigment, which allows our body to then reabsorb the ink and flush it out our lymphatic system. Our pricing is competitive and varies according to size. 
+				<p>The laser works to break down the pigment, which allows our body to then reabsorb the ink and flush it out our lymphatic system. Our pricing is competitive and varies according to size.</p>
 				<p>Our very well-versed laser technician operates our tattoo removal equipment and your appointment will only take a matter of minutes! To obtain a quote for your tattoo removal session, schedule a free cosmetic consultation to have the area measured at Canopy MD.</p> 
 				<p>View our before & after photos!</p>
 			</div>
